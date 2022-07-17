da131c4966ea4d5204fae511f94566fd789ec303
Tim Gymnich <tgymnich@icloud.com>
2022-07-17 18:09:59 +0200

    remove exports

--- src/PPX.jl
+++ src/PPX.jl
@@ -1,14 +1,7 @@
 module PPX
 
-
-export Handshake, HandshakeResult, Run, RunResult, Sample, SampleResult, Observe, ObserveResult, Tag, TagResult, Reset
-export Normal, Uniform, Categorical, Poisson, Bernoulli, Beta, Exponential, Gamma, LogNormal, Binomial, Weibull
-
 include("Distribution.jl")
 include("Message.jl")
 include("Tensor.jl")
 
-
 end # module
-
-
